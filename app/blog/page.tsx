558c3966584049fb04f9b310ba7945deee06dc37
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2025-02-06 11:19:57 +0530

    fixing type error on deployment

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,14 +1,14 @@
 import CardList from "@/components/CardList";
 import Menu from "@/components/Menu";
-import { FC } from "react";
 
 interface BlogPageProps {
-  searchParams: Record<string, string | undefined>;
+  searchParams: Promise<Record<string, string | undefined>>;
 }
 
-const BlogPage = ({ searchParams }: BlogPageProps) => {
-  const page = parseInt(searchParams.page as string) || 1;
-  const cat = searchParams.cat || "";
+const BlogPage = async ({ searchParams }: BlogPageProps) => {
+  const params = await searchParams;
+  const page = parseInt(params.page as string) || 1;
+  const cat = params.cat || "";
   return (
     <div>
       <h1 className="bg-orange-400 text-white py-1 px-3 text-2xl text-center font-semibold capitalize">
